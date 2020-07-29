c7c32f425cfe0ee72bcb495354e82b30f6b0bc6f
Tobi Okanlawon <tobiinlondon34@gmail.com>
2020-07-29 22:37:14 +0100

    first commit

---
+++ src/components/sideBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <h1 class="title">Crust</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.main {
+  background-color: #403838;
+  height: 100%;
+}
+.container {
+  text-align: center;
+  justify-content: center;
+  width: 100%;
+}
+.title {
+  color: #acf5f5;
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <sideBar />
+  </div>
+</template>
+
+<script>
+import { sideBar } from "@/components/sideBar.vue";
+export default {
+  components: {
+    sideBar: sideBar
+  }
+};
+</script>
+
+<style></style>
