772cd8fe9655486447bade2f3f63a0f938289b06
Tobi Okanlawon <tobiinlondon34@gmail.com>
2020-08-01 21:56:02 +0100

    Removed default Vue Home page. Added tailwind.

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,19 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/home">New Home</router-link>
+      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
 <style>
+@tailwind base;
+
+@tailwind components;
+
+@tailwind utilities;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
