89310af9abf3bd0654b0cbb4ce7b4e7c59e07b99
Tobi Okanlawon <tobiinlondon34@gmail.com>
2020-08-12 17:23:56 +0100

    Cleaned prop format

--- src/components/actions.vue
+++ src/components/actions.vue
@@ -3,8 +3,10 @@
     <button
       class="bg-gray-300 py-2 mx-auto w-full px-12 outline-none focus:outline-none hover:bg-gray-400 my-1 rounded-lg"
     >
-      <span class="mx-auto text-center text-lg text-gray-700">{{name.name}}</span>
-      <div class="mx-auto text-gray-500 text-sm w-full">{{ name.desc }}</div>
+      <!-- h-12 md:h24-->
+      <!-- Change the height of the button to accommodate for smaller screens.-->
+      <span class="mx-auto text-center text-lg text-gray-700">{{action.name}}</span>
+      <div class="mx-auto text-gray-500 text-sm w-full">{{ action.desc }}</div>
     </button>
   </div>
 </template>
@@ -12,9 +14,10 @@
 <script>
 export default {
   props: {
-    name,
+    action: {
+      type: Object,
+    },
   },
 };
 </script>
 
-<style></style>
