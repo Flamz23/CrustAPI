65b82b07d6e44170e615a03ac60c9a95bde020ea
Tobi Okanlawon <tobiinlondon34@gmail.com>
2020-08-12 17:31:11 +0100

    Added workspace component to the homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,18 +2,21 @@
   <div class="flex main w-full h-screen bg-black-500">
     <sideBar />
     <masthead />
+    <workspace />
   </div>
 </template>
 
 <script>
 import sideBar from "@/components/sideBar.vue";
 import Masthead from "@/components/masthead.vue";
+import workspace from "@/components/workspace.vue";
 
 export default {
   name: "HomePage",
   components: {
     masthead: Masthead,
     sideBar: sideBar,
+    workspace,
   },
 };
 </script>
