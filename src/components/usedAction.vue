085837ba54b1a2fcbb647e83c8825914238b42be
Tobi Okanlawon <tobiinlondon34@gmail.com>
2020-08-12 17:24:35 +0100

    Styled usedActions tabs

--- src/components/usedAction.vue
+++ src/components/usedAction.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-12">
-    <div class="bg-gray-300 py-2 my-2 mx-auto w-full rounded-lg">{{name.name}}</div>
+    <div class="px-4 bg-gray-300 py-2 my-2 mx-auto w-full rounded-lg">{{name.name}}</div>
   </div>
 </template>
 <script>
