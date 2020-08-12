43a65495c68315d4d2d2b0893b12c341f949dc1b
Tobi Okanlawon <tobiinlondon34@gmail.com>
2020-08-12 17:23:25 +0100

    Abstracted Workspace

--- src/components/actionHolder.vue
+++ src/components/actionHolder.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="absolute flex w-full">
-    <div class="inset-y-0 h-full w-1/4 pr-2">
+    <div class="inset-y-0 h-full pr-2">
       <div id="actionsHolder" class="bg-gray-600 overflow-y-scroll px-4 my-3">
         <draggable
           v-model="actions"
@@ -8,17 +8,10 @@
           :sort="false"
         >
           <div v-for="action in actions" :key="action.id">
-            <actions :name="action" />
+            <actions :action="action" />
           </div>
         </draggable>
       </div>
-    </div>
-    <div class="flex-1">
-      <draggable v-model="usedActions" :group="{name: 'actions-list'}" :animation="200">
-        <div class="px-32" v-for="usedAction in usedActions" :key="genKey(usedAction.name)">
-          <usedActions :name="usedAction" />
-        </div>
-      </draggable>
     </div>
   </div>
 </template>
@@ -26,54 +19,50 @@
 <script>
 import draggable from "vuedraggable";
 import actions from "@/components/actions.vue";
-import usedActions from "@/components/usedAction.vue";
-
-const actionArray = [
-  {
-    name: "Text",
-    desc: "A simple text field to enter info",
-    id: 2,
-  },
-  {
-    name: "Notification",
-    desc: "Set up mock notifications for your computer",
-    id: 1,
-  },
-  {
-    name: "Brightness",
-    desc: "Get and set the brightness of your computer",
-    id: 3,
-  },
-  {
-    name: "Volume",
-    desc: "Get and Set your device volume",
-    id: 4,
-  },
-  {
-    name: "Info",
-    desc: "Set up a mock information dialog for error messages",
-    id: 5,
-  },
-  {
-    name: "Error",
-    desc: "lorem Ipsum dolor sit amet ci avec le dos les avoir les yeux",
-    id: 6,
-  },
-  {
-    name: "Seventh Actions",
-    id: 7,
-  },
-];
 
 export default {
   components: {
     actions,
     draggable,
-    usedActions,
   },
   data() {
     return {
-      actions: actionArray,
+      actions: [
+        {
+          name: "Text",
+          desc: "A simple text field to enter info",
+          id: 2,
+        },
+        {
+          name: "Notification",
+          desc: "Set up mock notifications for your computer",
+          id: 1,
+        },
+        {
+          name: "Brightness",
+          desc: "Get and set the brightness of your computer",
+          id: 3,
+        },
+        {
+          name: "Volume",
+          desc: "Get and Set your device volume",
+          id: 4,
+        },
+        {
+          name: "Info",
+          desc: "Set up a mock information dialog for error messages",
+          id: 5,
+        },
+        {
+          name: "Error",
+          desc: "lorem Ipsum dolor sit amet ci avec le dos les avoir les yeux",
+          id: 6,
+        },
+        {
+          name: "Seventh Actions",
+          id: 7,
+        },
+      ],
       usedActions: [
         {
           name: "Text",
@@ -83,12 +72,7 @@ export default {
       ],
     };
   },
-  methods: {
-    // this is a random no. gen.
-    genKey: (len) => {
-      return Math.random().toString(36).substring(7);
-    },
-  },
+  methods: {},
 };
 </script>
 
