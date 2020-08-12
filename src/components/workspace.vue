9a8156fceeb3f65372d3d2dab812be050551711c
Tobi Okanlawon <tobiinlondon34@gmail.com>
2020-08-12 17:32:24 +0100

    Abstracted worspace

---
+++ src/components/workspace.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class>
+    <draggable v-model="usedActions" :group="{name: 'actions-list'}" :animation="200">
+      <div class v-for="usedAction in usedActions" :key="usedAction.id">
+        <usedActions :name="usedAction" />
+      </div>
+    </draggable>
+  </div>
+</template>
+
+<script>
+import usedActions from "@/components/usedAction.vue";
+import draggable from "vuedraggable";
+
+export default {
+  components: {
+    usedActions,
+    draggable,
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    usedActions() {
+      return this.$store.getters.getUsedActions;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
